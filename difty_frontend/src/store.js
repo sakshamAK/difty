7316f04a40f995b861adcad318fea8fa0a560204
Saksham <55906931+sakshamAK@users.noreply.github.com>
2023-11-11 09:01:00 +0530

    empty

--- difty_frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import gptReducer from "./redux/slices/generateTextSlice";
-
-export const store = configureStore({
-    reducer: {
-        gptContent: gptReducer
-    }
-})
